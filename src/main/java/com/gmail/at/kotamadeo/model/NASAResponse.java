898c44decb23af75428574c7167ab179a13daff3
kotamadeo <kotamadeo@gmail.com>
2022-08-16 21:14:10 +0300

    minor fixes in model.

--- src/main/java/com/gmail/at/kotamadeo/model/NASAResponse.java
+++ src/main/java/com/gmail/at/kotamadeo/model/NASAResponse.java
@@ -5,8 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -25,6 +23,6 @@ public class NASAResponse {
 
     @Override
     public String toString() {
-        return String.format("%s. %s.%n%s ©%s", title, date, explanation, copyright);
+        return String.format("%s. %s.%n%s ©%s.", title, date, explanation, copyright);
     }
 }
